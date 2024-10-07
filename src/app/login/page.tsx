0153685f7d65a04c147fff5cc46bc7e8bbd9dac6
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-10-08 00:16:44 +0530

    Verify and login page designed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+'use client'
 
-function page() {
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+
+export default function SignupPage() {
+  const router = useRouter()
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  })
+
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const onSignup = async () => {
+    try {
+      setLoading(true)
+      setButtonDisabled(true)
+      const response = await axios.post("/api/users/login", user)
+      console.log("Login success", response.data);
+      router.push("/login")
+    } catch (error: any) {
+      console.log("Signup failed");
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0 ) {
+      setButtonDisabled(false)
+    } else {
+      setButtonDisabled(true)
+    }
+  }, [user])
+
   return (
-    <div>
-      Login Page
+    <div className='flex flex-col justify-center items-center min-h-screen'>
+      <div className='flex flex-col'>
+        <h1 className='text-3xl font-medium mb-8 text-center'>{loading ? "Processing" : "Login"}</h1>
+        <label className='text-left mb-2' htmlFor="username">Email</label>
+        <input className='p-2 rounded-md text-black' type="text" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder='Email' /> <br />
+        <label className='text-left mb-2' htmlFor="username">Password</label>
+        <input className='p-2 rounded-md text-black' type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder='Password' /> <br />
+        <button className='px-3 py-2 bg-blue-600 rounded-md w-max mx-auto font-medium -tracking-tighter hover:bg-blue-700 transition-all active:bg-blue-800 disabled:bg-blue-400 disabled:text-gray-300 disabled:cursor-not-allowed' disabled= {buttonDisabled} onClick={onSignup}>Login</button>
+        <Link href="/signup" className='text-center my-4'>Visit Sign Up Page </Link>
+      </div>
     </div>
   )
 }
-
-export default page
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+'use client'
 
-function page() {
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+
+export default function VerifyEmailPage() {
+  // const router = useRouter()
+  const [token, setToken] = useState("")
+  const [verified, setVerified] = useState(false)
+  const [error, setError] = useState(false)
+
+  const verifyUserEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", {token})
+      setVerified(true)
+      setError(false)
+    } catch (error: any) {
+      setError(true)
+      console.log(error.response.data);
+      toast.error(error.response.data)
+    }
+  }
+
+  useEffect(() => {
+    setError(false)
+    const urlToken = window.location.search.split("=")[1]
+    setToken(urlToken || "")
+
+    // const { query } = router
+    // const urlToken = query.token
+    // setToken(urlToken?.toString || "")
+  }, [])
+
+  useEffect(() => {
+    setError(false)
+    if (token.length > 0) {
+      verifyUserEmail()
+    }
+  }, [token])
+
   return (
-    <div>
-      
+    <div className='flex flex-col justify-center items-center min-h-screen'>
+      <h1 className='text-3xl font-bold mb-4'>Verify Email</h1>
+      <h2 className='bg-orange-500 text-black p-2'>{token? `${token}`: "No token"}</h2>
+      {verified && (
+        <h2 className='px-3 py-2 my-4 bg-green-500 rounded-md'>Verified</h2>
+      )}
+      {error && (
+        <h2 className='px-3 py-2 my-4 bg-red-500 rounded-md'>Error</h2>
+      )}
     </div>
   )
 }
-
-export default page
