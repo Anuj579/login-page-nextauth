d348b1196b3683315b9b2e8462d4b44ca268029d
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-10-09 21:34:00 +0530

    login page designed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
-export default function SignupPage() {
+export default function LoginPage() {
   const router = useRouter()
   const [user, setUser] = useState({
     email: "",
@@ -16,15 +16,15 @@ export default function SignupPage() {
   const [buttonDisabled, setButtonDisabled] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  const onSignup = async () => {
+  const onLogin = async () => {
     try {
       setLoading(true)
       setButtonDisabled(true)
       const response = await axios.post("/api/users/login", user)
       console.log("Login success", response.data);
-      router.push("/login")
+      router.push("/profile")
     } catch (error: any) {
-      console.log("Signup failed");
+      console.log("Login failed");
       toast.error(error.message)
     }
   }
@@ -45,7 +45,7 @@ export default function SignupPage() {
         <input className='p-2 rounded-md text-black' type="text" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder='Email' /> <br />
         <label className='text-left mb-2' htmlFor="username">Password</label>
         <input className='p-2 rounded-md text-black' type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder='Password' /> <br />
-        <button className='px-3 py-2 bg-blue-600 rounded-md w-max mx-auto font-medium -tracking-tighter hover:bg-blue-700 transition-all active:bg-blue-800 disabled:bg-blue-400 disabled:text-gray-300 disabled:cursor-not-allowed' disabled= {buttonDisabled} onClick={onSignup}>Login</button>
+        <button className='px-3 py-2 bg-blue-600 rounded-md w-max mx-auto font-medium -tracking-tighter hover:bg-blue-700 transition-all active:bg-blue-800 disabled:bg-blue-400 disabled:text-gray-300 disabled:cursor-not-allowed' disabled= {buttonDisabled} onClick={onLogin}>Login</button>
         <Link href="/signup" className='text-center my-4'>Visit Sign Up Page </Link>
       </div>
     </div>
